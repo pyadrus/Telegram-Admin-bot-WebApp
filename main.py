d5aff1b2ce0a08f0c440a0c57c7107ffcc9646c3
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-12-13 16:36:14 +0300

    Миграция с aiogram2 на aiogram3. Рефакторинг кода. Структурирование проекта.

--- main.py
+++ main.py
@@ -2,21 +2,13 @@ import asyncio
 import logging
 import sys
 
-from aiogram import types
-from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
 from loguru import logger
 
 from handlers.admin_handlers import admin_handlers
 from system.dispatcher import bot
 from system.dispatcher import dp
-from system.dispatcher import router
 
 logger.add("setting/log/log.log", rotation="1 MB", compression="zip")
-
-
-
 
 
 async def main():
