e8de6c2719a16e9d189e687cf05ec0a79779d0d9
PyAdminRU <zh.vitaliy92@gmail.com>
2025-05-10 12:12:42 +0300

    Рефакторинг кода

--- states/states.py
+++ states/states.py
@@ -1,11 +1,6 @@
 from aiogram.fsm.state import StatesGroup, State
 
 
-class AddUserStates(StatesGroup):
-    WAITING_FOR_USER_ID = State()  # ожидание ввода ID пользователя;
-    USER_ADDED = State()  # состояние, когда пользователь успешно добавлен в базу данных.
-
-
 class AddAndDelBadWords(StatesGroup):
     """Создаем состояние для добавления плохих слов"""
     waiting_for_bad_word = State()
