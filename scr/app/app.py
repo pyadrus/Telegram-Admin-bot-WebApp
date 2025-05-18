479620d56860d7cab8337f3bf19e2fe50ce97605
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-05-18 20:39:01 +0300

    Функция "Ограничение по подписке на канал", вынесена на отдельную страницу.

--- scr/app/app.py
+++ scr/app/app.py
@@ -40,6 +40,12 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
+# Новый маршрут для "Ограничение по подписке на канал"
+@app.get("/channel_subscription_limit")
+async def channel_subscription_limit(request: Request):
+    return templates.TemplateResponse("channel_subscription_limit.html", {"request": request})
+
+
 # Новый маршрут для "Фильтр запрещённых слов"
 @app.get("/filter_words")
 async def filter_words(request: Request):
