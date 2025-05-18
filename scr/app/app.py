6f174365a4bd4237ab9dfe5b50e3a5f84fd64d51
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-05-18 20:58:32 +0300

    Функция "Количество участников", вынесена на отдельную страницу.

--- scr/app/app.py
+++ scr/app/app.py
@@ -40,6 +40,12 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
+# Новый маршрут для "Количество участников"
+@app.get("/restrictions_on_messages")
+async def restrictions_on_messages(request: Request):
+    return templates.TemplateResponse("restrictions_on_messages.html", {"request": request})
+
+
 # Новый маршрут для "Ограничения на сообщения"
 @app.get("/restrictions_messages")
 async def restrictions_messages(request: Request):
